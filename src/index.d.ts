55e9fcc5dff94072a4393f263f2ad9d41f614f76
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-09-07 06:58:59 -0400

    fix: types, closes #2

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,5 +1,8 @@
 import { Interception } from 'cypress/types/net-stubbing'
 
+/**
+ * Options for the cy.waitIfHappens commands
+ */
 interface WaitIfHappensOptions {
   alias: string
   timeout?: number
@@ -7,23 +10,28 @@ interface WaitIfHappensOptions {
   yieldResponseBody?: boolean
 }
 
-declare namespace Cypress {
-  interface Chainable {
-    /**
-     * Waits up to the timeout (ms) for the given network intercept to happen.
-     * If the call never happens, yields undefined
-     */
-    waitIfHappens(
-      alias: string,
-      timeout?: number,
-    ): Chainable<Interception | undefined>
-    /**
-     * Waits up to the timeout (ms) for the given network intercept to happen,
-     * can yield the last call for the current calls, and yield the response body.
-     * If the call never happens, yields undefined
-     */
-    waitIfHappens(
-      options: WaitIfHappensOptions,
-    ): Chainable<Interception | undefined>
+declare global {
+  namespace Cypress {
+    interface Chainable {
+      /**
+       * Waits up to the timeout (ms) for the given network intercept to happen.
+       * If the call never happens, yields undefined.
+       * @example cy.waitIfHappens('@loads', 1000)
+       * @see https://github.com/bahmutov/cypress-wait-if-happens
+       */
+      waitIfHappens(
+        alias: string,
+        timeout?: number,
+      ): Chainable<Interception | undefined>
+      /**
+       * Waits up to the timeout (ms) for the given network intercept to happen,
+       * can yield the last call for the current calls, and yield the response body.
+       * If the call never happens, yields undefined
+       * @see https://github.com/bahmutov/cypress-wait-if-happens
+       */
+      waitIfHappens(
+        options: WaitIfHappensOptions,
+      ): Chainable<Interception | undefined>
+    }
   }
 }
